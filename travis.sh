fee4d55737b0d9c09a9c4fd4514c2b9b5c1d7762
Jerrin Shaji George <jshajigeorge@vmware.com>
2019-08-14 23:18:56 +0000

    Add travis CI scripts
    Signed-off-by: Jerrin Shaji George <jshajigeorge@vmware.com>

---
+++ travis.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# Travis CI clones the repository at ~/vmware/go-pmem-transaction
+# Move the repository into GOPATH first.
+cd ..
+mkdir -p $GOPATH/src/github.com/vmware
+mv go-pmem-transaction $GOPATH/src/github.com/vmware
+
+# Build the modified Go compiler.
+git clone --depth 1  https://github.com/jerrinsg/go-pmem ~/go-pmem
+cd ~/go-pmem/src
+./make.bash
+
+cd $GOPATH/src/github.com/vmware/go-pmem-transaction/txtest
+
+# Force travis CI to use the compiler and toolchain we just built.
+# If the test is run as ~/go-pmem/bin/go test, travis is using the tools found
+# in /home/travis/.gimme/versions/go1.11.1.linux.amd64 to do the build (TODO).
+GOROOT="$HOME/go-pmem/" GOTOOLDIR="$HOME/go-pmem/pkg/tool/linux_amd64" ~/go-pmem/bin/go test
