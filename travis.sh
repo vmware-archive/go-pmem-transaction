36d1f2786fff4c18582024e01810d8aea1ee1e96
Jerrin Shaji George <jshajigeorge@vmware.com>
2019-08-30 23:41:27 +0000

    Make Travis CI run the pmem test suite as well
    Signed-off-by: Jerrin Shaji George <jshajigeorge@vmware.com>

--- travis.sh
+++ travis.sh
@@ -13,9 +13,12 @@ git clone --depth 1  https://github.com/jerrinsg/go-pmem ~/go-pmem
 cd ~/go-pmem/src
 ./make.bash
 
-cd $GOPATH/src/github.com/vmware/go-pmem-transaction/txtest
-
 # Force travis CI to use the compiler and toolchain we just built.
 # If the test is run as ~/go-pmem/bin/go test, travis is using the tools found
 # in /home/travis/.gimme/versions/go1.11.1.linux.amd64 to do the build (TODO).
+
+cd $GOPATH/src/github.com/vmware/go-pmem-transaction/txtest
+GOROOT="$HOME/go-pmem/" GOTOOLDIR="$HOME/go-pmem/pkg/tool/linux_amd64" ~/go-pmem/bin/go test
+
+cd $GOPATH/src/github.com/vmware/go-pmem-transaction/pmemtest
 GOROOT="$HOME/go-pmem/" GOTOOLDIR="$HOME/go-pmem/pkg/tool/linux_amd64" ~/go-pmem/bin/go test
