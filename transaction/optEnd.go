50ecbdb73b5d9918df0979294ab6bf1fc28000b1
Jerrin Shaji George <jshajigeorge@vmware.com>
2019-12-11 10:59:18 -0800

    Using a map instead of an RB Tree

--- transaction/optEnd.go
+++ transaction/optEnd.go
@@ -9,29 +9,43 @@ var cacheLineSz = uintptr(runtime.FLUSH_ALIGN)
 
 type (
 	flushSt struct {
-		data *Tree
+		//data *Tree
+		data map[uintptr]int
 	}
 )
 
 func (f *flushSt) insert(start, size uintptr) {
 	if f.data == nil {
-		f.data = NewRbTree()
+		//f.data = NewRbTree()
+		f.data = make(map[uintptr]int)
 	}
+
 	alignedAddr := start &^ (cacheLineSz - 1)
-	if f.data.Get(alignedAddr) {
-		return // already exists
-	}
+	//f.data[alignedAddr] = 1
+	//return
+
+	//if f.data.Get(alignedAddr) {
+		//	return // already exists
+	//}
 	// We only care about cacheline aligned addresses
 	for alignedAddr < start+size {
-		f.data.Put(alignedAddr)
+		//f.data.Put(alignedAddr)
+		f.data[alignedAddr] = 1
 		alignedAddr += cacheLineSz
 	}
 }
 
 func (f *flushSt) flushAndDestroy() {
 	if f.data != nil {
-		flushRbTree(f.data.Root)
+		//flushRbTree(f.data.Root)
+		flushRbTreeMap(f.data)
 		f.data = nil
+	}
+}
+
+func flushRbTreeMap(m map[uintptr]int) {
+	for k,_ := range m {
+		runtime.FlushRange(unsafe.Pointer(k), cacheLineSz)	
 	}
 }
 
